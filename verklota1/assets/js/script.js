9c0df2b263470a419bab5d05f711d35e3577b41d
mikkikalla <mikkikalla@MacBook-Pro-2.local>
2017-02-17 12:58:40 +0000

    adding additional JS functions

--- verklota1/assets/js/script.js
+++ verklota1/assets/js/script.js
@@ -1,15 +1,52 @@
-$(document).ready(function(){
-	$('.superslider').slick({
-		arrows: false,
-		dots: true
-	});
+let r = new XMLHttpRequest();
+r.open("GET", "https://api.themoviedb.org/3/discover/movie?api_key=fdf806fd1275ea7699d8e2ad53e054e2");
+r.onreadystatechange = function () {
+  if (r.readyState != 4 || r.status != 200) return;
 
-});
+  // const getGenre = new XMLHttpRequest();
+  // r.open("GET", "https://api.themoviedb.org/3/genre/movie/list?api_key=fdf806fd1275ea7699d8e2ad53e054e2&language=en-US", true);
+  // r.onreadystatechange = function () {
+  // if (r.readyState != 4 || r.status != 200) return;
+  
+  const response = JSON.parse(r.responseText);
+  
+  let movieList = document.querySelector('.popular-list')
 
-$('.responsive').slick({
+  const IdInfo = new generalInfo();
+
+  for (let movie of response.results) {
+
+
+    console.log(movie);
+    const image_path = "http://image.tmdb.org/t/p/original"; //poster path
+    const poster_loc = image_path + movie.poster_path;
+    const movie_title = movie.title;
+    const rating = movie.vote_average;
+    const genre = movie.genre_ids;
+    const getGenre = IdInfo.getGenres(genre);
+
+
+
+    let movieStr = `\
+      <div class="slide-content image"><a href="#"><img src="${poster_loc}"></a> 
+        <div class="textOverlayBg">
+          <div class="skew"> 
+            <h3 class="textOverlayH3">${movie_title}</h3> 
+            <a class="textOverlayA" href="#">Felicity Jones | Diego Luna | Forest Whitaker</a> 
+            <p class="textOverlayP">${getGenre}</p> 
+            <div class="textOverlayC circleBase type2"><h4 class="rating"><strong>${rating}</strong></h4></div> 
+          </div> 
+        </div> 
+      </div>
+    `;
+
+    movieList.innerHTML += movieStr;
+
+  }
+
+$(movieList).slick({
   arrows: true,
-  dots: false,
-  infinite: false,
+  dots: false,    infinite: true,
   speed: 300,
   slidesToShow: 4,
   slidesToScroll: 2,
@@ -18,33 +55,107 @@ $('.responsive').slick({
     {
       breakpoint: 1024,
       settings: {
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        infinite: true,
-        arrows: false,
-        dots: false
+         slidesToShow: 3.05,
+         slidesToScroll: 1,
+         arrows: false,
+         dots: false
       }
     },
     {
       breakpoint: 600,
       settings: {
-        slidesToShow: 2,
+        slidesToShow: 2.05,
         slidesToScroll: 1,
         arrows: false,
         dots: false
       }
     },
     {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: false,
-        dots: false
-      }
+       breakpoint: 480,
+       settings: {
+         slidesToShow: 1.05,
+         slidesToScroll: 1,
+         arrows: false,
+         dots: false
+       }
     }
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
   ]
 });
+
+};
+r.send();
+
+
+class generalInfo { 
+  constructor(movieID) {
+    this.cast = " ";
+    this.genres = " ";
+    this.movideID = movieID;
+
+  }
+  getGenres(IDs){
+   let movieGenres = [ ]; 
+    movieGenres = genres.filter(function(genre){
+      return IDs.indexOf(genre.id) > -1;
+    });
+    console.log(movieGenres);
+    let genreString = " ";
+    movieGenres.forEach(function(movieGenre){
+      genreString += movieGenre.name + ", ";
+    });
+    return genreString;    
+  }
+ } 
+
+// $(document).ready(function(){
+// 	$('.superslider').slick({
+// 		arrows: false,
+// 		dots: true
+// 	});
+
+// });
+
+// $('.responsive').slick({
+//   arrows: true,
+//   dots: false,
+//   infinite: true,
+//   speed: 300,
+//   slidesToShow: 4,
+//   slidesToScroll: 2,
+
+//   responsive: [
+//     {
+//       breakpoint: 1024,
+//       settings: {
+//         slidesToShow: 3.05,
+//         slidesToScroll: 1,
+//         arrows: false,
+//         dots: false
+//       }
+//     },
+//     {
+//       breakpoint: 600,
+//       settings: {
+//         slidesToShow: 2.05,
+//         slidesToScroll: 1,
+//         arrows: false,
+//         dots: false
+//       }
+//     },
+//     {
+//       breakpoint: 480,
+//       settings: {
+//         slidesToShow: 1.05,
+//         slidesToScroll: 1,
+//         arrows: false,
+//         dots: false
+//       }
+//     }
+//     // You can unslick at a given breakpoint now by adding:
+//     // settings: "unslick"
+//     // instead of a settings object
+//   ]
+// });
